20cf6d8ea51fb747ece2d19d3f489df7e582303f
Adam Tester <adam@genyx.co.uk>
2015-12-14 18:38:43 +0000

    Remove CORS protection

--- index.php
+++ index.php
@@ -30,10 +30,38 @@ class App {
 	 */
 	public function boot()
 	{
+		$this->setCORS();
 		$this->setConfig();
 		$this->subscribe();
 
 		echo 'OK';
+	}
+
+	/**
+	 *
+	 * Allow POST from anywhere with CORS
+	 *
+	 */
+	private function setCORS() {
+
+		// Allow from any origin
+		if (isset($_SERVER['HTTP_ORIGIN'])) {
+			header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+			header('Access-Control-Allow-Credentials: true');
+			header('Access-Control-Max-Age: 86400');    // cache for 1 day
+		}
+
+		// Access-Control headers are received during OPTIONS requests
+		if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+			if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+				header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+			if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+				header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+			exit(0);
+		}
 	}
 
 	/**
@@ -55,7 +83,7 @@ class App {
 		if(!$this->privateKey) {
 			$error = true;
 		}
-		
+
 		if(isset($_POST['domain'])) {
 			$this->domain = $_POST['domain'];
 		} else {
